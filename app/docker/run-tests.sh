64ce0b23ced09f0a6797e4c293c609ec1eef95b6
WillOrchard <will.orchard@solirius.com>
2023-03-21 15:01:58 +0000

    Created Dockerfile and Jenkinsfile

---
+++ app/docker/run-tests.sh
@@ -0,0 +1,6 @@
+
+
+#!/usr/bin/env bash
+
+npx codeceptjs run --steps
+ls report
---
+++ app/jenkins/run-tests.sh
@@ -0,0 +1,13 @@
+VERSION=${1:-latest}
+
+echo "Pulling image ${VERSION}"
+mkdir report
+
+docker run --rm \
+    -v "$(pwd)"/report/:/app/report/ \
+    peterngtr/rest-demo:${VERSION}
+
+status=$?
+
+echo "Final status ${status}"
+exit ${status}
